82fd941d96d5468bbc9a15a326b0b3f6f6a1ba51
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-10 07:47:43 -0500

    grid global

---
+++ src/sphis/grid/GridGlobal.hx
@@ -0,0 +1,23 @@
+package src.sphis.grid;
+
+class GridManager {
+	public static var GRID_WIDTH:Int = 8;
+	public static var GRID_HEIGHT:Int = 8;
+
+	/**
+	 * This will set both `GRID_WIDTH` and `GRID_HEIGHT` through 1 argument
+	 */
+	public static function setGridSizePack(packSize:Int)
+        {
+                setGridSize(packSize, packSize);
+        }
+        
+	/**
+	 * This will set both `GRID_WIDTH` and `GRID_HEIGHT` through 2 arguments
+	 */
+	public static function setGridSize(width:Int,height:Int)
+        {
+                GRID_WIDTH = width;
+                GRID_HEIGHT = height;
+        }
+}
